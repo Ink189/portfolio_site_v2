46d3f1742c37cdc018f72a4dcb33d9d79853ca41
Ink189 <petindanil46@gmail.com>
2026-02-25 15:35:21 +0500

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    
+    themeToggleButton.addEventListener('click', () => {
+
+        document.body.classList.toggle('dark-theme');
+    });
+});
